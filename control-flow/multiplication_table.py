b4b16f5f24c3ced49da13be6954c364a7fef3b44
WuorBhang <uhuribhang211@gmail.com>
2024-06-26 14:47:12 +0300

    adding multiplication files

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,7 +1,8 @@
 #prompt user number
 number = int(input("Enter a number to see its multiplication table. "))
 
-#generate and print the multiplication table numbers
+
+#looping from 1 to 10
 for i in range(1, 10 + 1):
    product = number * i
    print(f"{number} * {i} = {product}")
