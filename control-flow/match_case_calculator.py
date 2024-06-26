7e3d93a4ad7080b7d057acb430d797480c0c27ae
WuorBhang <uhuribhang211@gmail.com>
2024-06-26 14:28:59 +0300

    adding match casing file

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -6,15 +6,15 @@ operation = operation = input("Choose the operation (+, -, *, /): ").strip()
 match operation:
         case '+':
             result = num1 + num2
-            print(f"The result of {num1} + {num2} is {result}.")
+            print(f"The result is {result}.")
         
         case '-':
             result = num1 - num2
-            print(f"The result of {num1} - {num2} is {result}.")
+            print(f"The result is {result}.")
         
         case '*':
           result = num1 * num2
-          print(f"The result of {num1} * {num2} is {result}.")
+          print(f"The result is {result}.")
         
         case '/':
             if num2 != 0:
