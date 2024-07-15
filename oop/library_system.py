97388c959fbfecf46efc27e11eeefd9efbd65d65
WuorBhang <uhuribhang211@gmail.com>
2024-07-15 20:17:56 +0300

    adding library files

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,20 +1,24 @@
 # library_system.py
+
 class Book:
-    #base class representing a book with title and author.
+    
     def __init__(self, title, author):
         self.title = title
         self.author = author
 
     def __str__(self):
-        return f"{self.title} by {self.author}"
+        return f"Book: {self.title} by {self.author}"
+
 
 class EBook(Book):
+  
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
 
     def __str__(self):
-        return f"{self.title} by {self.author}, File Size: {self.file_size}KB"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
 
 class PrintBook(Book):
    
@@ -23,17 +27,19 @@ class PrintBook(Book):
         self.page_count = page_count
 
     def __str__(self):
-        return f"{self.title} by {self.author}, Page Count: {self.page_count}"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
 
 class Library:
+   
     def __init__(self):
         self.books = []
 
     def add_book(self, book):
-        #Add a book to the library collection
+       
         self.books.append(book)
 
     def list_books(self):
-        #List all books in the library
+   
         for book in self.books:
             print(book)
