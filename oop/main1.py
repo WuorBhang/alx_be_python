95af0f1566966ef2f8c17d7d5763472d2d18cc87
WuorBhang <uhuribhang211@gmail.com>
2024-07-15 20:12:38 +0300

    adding library files

--- oop/main1.py
+++ oop/main1.py
@@ -1,21 +1,22 @@
+# main.py
+
 from library_system import Book, EBook, PrintBook, Library
 
 def main():
-    # Create a Library instance
-    my_library = Library()
+    library = Library()
+    
+    # Adding books to the library
+    book1 = Book("Pride and Prejudice", "Jane Austen")
+    ebook1 = EBook("Snow Crash", "Neal Stephenson", 500)
+    printbook1 = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
+    
+    library.add_book(book1)
+    library.add_book(ebook1)
+    library.add_book(printbook1)
 
-    # Create instances of each type of book
-    classic_book = Book("Pride and Prejudice", "Jane Austen")
-    digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
-    paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
-
-    # Add books to the library
-    my_library.add_book(classic_book)
-    my_library.add_book(digital_novel)
-    my_library.add_book(paper_novel)
-
-    # List all books in the library
-    my_library.list_books()
+    # Listing all books in the library
+    print("Listing all books in the library:")
+    library.list_books()
 
 if __name__ == "__main__":
     main()
